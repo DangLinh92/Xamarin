31156ce3d76debfc64e34bc60a386a1718ad664e
danglv <danglevan.9919@gmail.com>
2021-12-17 14:42:37 +0700

    init project Smart Gas

---
+++ GAS APP WHC/SmartGas/SmartGas/SmartGas/Services/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using SmartGas.ViewModels;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartGas.Services
+{
+    public interface INavigationService
+    {
+        Task NavigateToAsync<TViewModel>() where TViewModel : BaseViewModel;
+
+        Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : BaseViewModel;
+
+        Task GoBackAsync();
+    }
+}
---
+++ GAS APP WHC/SmartGas/SmartGas/SmartGas/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SmartGas.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartGas.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewItemPage : ContentPage
+    {
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
