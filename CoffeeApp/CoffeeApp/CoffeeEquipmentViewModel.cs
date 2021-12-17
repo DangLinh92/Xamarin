9947e86f822834058aca2c50e743ac9953e3c5dd
DangLinh92 <hoamoclan.3192@gmail.com>
2021-12-17 21:07:23 +0700

    up coffee App

---
+++ CoffeeApp/CoffeeApp/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,117 @@
+﻿using CoffeeApp.Model;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Command = MvvmHelpers.Commands.Command;
+
+namespace CoffeeApp
+{
+    public class CoffeeEquipmentViewModel : ViewModelBase
+    {
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string,Coffee>> CoffeeGroups { get; }
+
+        public AsyncCommand RefreshCommand { get; }
+
+        public AsyncCommand<Coffee> FavoriteCommand { get; }
+
+        public AsyncCommand<object> SelectedCommand { get; }
+
+        public Command LoadMoreCommand { get; }
+        public Command DelayLoadMoreCommand { get;}
+        public Command ClearCommand { get; }
+
+        Coffee selectedCoffee;
+        public Coffee SelectedCoffee
+        {
+            get => selectedCoffee;
+            set => SetProperty(ref selectedCoffee, value);
+        }
+
+        public CoffeeEquipmentViewModel()
+        {
+            Title = "Coffee Equipment";
+
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            LoadMore();
+
+            RefreshCommand = new AsyncCommand(Refresh);
+            FavoriteCommand = new AsyncCommand<Coffee>(Favorite);
+            LoadMoreCommand = new Command(LoadMore);
+            ClearCommand = new Command(Clear);
+            DelayLoadMoreCommand = new Command(DelayLoadMore);
+            SelectedCommand = new AsyncCommand<object>(Selected);
+        }
+
+        private void LoadMore() {
+            if (Coffee.Count >= 20)
+                return;
+            var image = "coffeebag.png";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+
+            CoffeeGroups.Clear();
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", Coffee.Where(c => c.Roaster == "Blue Bottle")));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Yes Plz", Coffee.Where(c => c.Roaster == "Yes Plz")));
+        }
+
+        private void DelayLoadMore()
+        {
+            if (Coffee.Count <= 10)
+                return;
+
+            LoadMore();
+        }
+
+        private void Clear()
+        {
+            Coffee.Clear();
+            CoffeeGroups.Clear();
+        }
+
+        private async Task Refresh()
+        {
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            Coffee.Clear();
+            LoadMore();
+
+            IsBusy = false;
+        }
+
+        async Task Favorite(Coffee coffee)
+        {
+            if (coffee == null)
+                return;
+
+            await Application.Current.MainPage.DisplayAlert("Favorite", coffee.Name, "OK");
+
+        }
+
+        async Task Selected(object args)
+        {
+            var coffee = args as Coffee;
+            if (coffee == null)
+                return;
+
+            SelectedCoffee = null;
+
+            await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
+            //await Application.Current.MainPage.DisplayAlert("Selected", coffee.Name, "OK");
+        }
+    }
+}
